a8646ccf3ee7e0047495ef567de280c5ec38b95a
VitaliiKyrpach <vitalij.9f0x9@gmail.com>
2023-07-15 23:20:09 +0300

    finish homework

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const formInput = document.querySelector('.feedback-form');
+formInput.addEventListener('input', throttle(onInput, 500));
+formInput.addEventListener('submit', onSubmit);
+
+updateOutput();
+
+function onInput() {
+  const inputValue = {
+    email: formInput.elements.email.value,
+    message: formInput.elements.message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputValue));
+}
+
+function updateOutput() {
+  const output = localStorage.getItem('feedback-form-state');
+  const parsedOutput = JSON.parse(output);
+  if (!parsedOutput) {
+    formInput.elements.email.value = '';
+    formInput.elements.message.value = '';
+  } else {
+    formInput.elements.email.value = parsedOutput.email;
+    formInput.elements.message.value = parsedOutput.message;
+  }
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const getData = localStorage.getItem('feedback-form-state');
+  console.log(JSON.parse(getData));
+  localStorage.removeItem('feedback-form-state');
+  evt.target.reset();
+}
